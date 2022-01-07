852f83621f853ab9dc796718335563e223626bc4
Delyan Ninov <dninov@gmail.com>
2022-01-07 07:24:54 +0200

    ic

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
+import { ActivatedRoute, Params } from "@angular/router";
+
+@Component({
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"],
+})
+export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.recipe = this.recipeService.getRecipe(+params["id"]);
+      console.log(this.recipe);
+    });
+  }
+  onAddToShoppingList() {
+    this.recipeService.addIngredientToShoppingList(this.recipe.ingredients);
+  }
+}
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,34 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { Recipe } from "./recipe.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      "Burger",
+      "A tasty burger recipe",
+      "https://upload.wikimedia.org/wikipedia/commons/1/11/Cheeseburger.png",
+      [new Ingredient("Meat Paddy", 1), new Ingredient("French Fries", 10)]
+    ),
+    new Recipe(
+      "Schintzel",
+      "A tasty schintzel recipe",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/220px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 10)]
+    ),
+  ];
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
+  addIngredientToShoppingList(ingredient: Ingredient[]) {
+    this.slService.addIngredients(ingredient);
+  }
+}
